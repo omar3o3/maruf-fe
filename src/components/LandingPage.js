d6f1d2cf357e990c27a6a8a4434f858607403d41
omar3o3 <otantawy53@gmail.com>
2023-03-19 15:02:37 -0400

    background image now has responsive css

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,43 +2,49 @@ import React from "react";
 import Typography from "@mui/material/Typography";
 import bgImage from "../images/main-bg.JPG";
 import bgImage2 from "../images/bg-pic.png";
+import test_image from "../images/test_image.jpeg"
 import {
     createTheme,
     ThemeProvider,
     styled,
-    responsiveFontSizes,
 } from "@mui/material/styles";
 
 function LandingPage() {
 
     return (
-    <div id="landing-page" >
-        <div>
-        <Typography
-            variant="h2"
-            align="center"
-            style={{ marginTop: "7%" }}
-        >
-            Maruf Community
-        </Typography>
-        <Typography variant="h6" align="center"
-            style={{
-                paddingLeft: "3%",
-                paddingRight: "3%",
-                marginTop: "3%",
-                fontWeight: "bold",
-            }}>
-            We chose the word "Maruf" to be the name of our community because at
-            the core it means something that is well known to be good, which is
-            what we strive for. The prophetic teachings, healthy social and
-            learning environment, and most importantly the genuine companionship
-            developed and experienced at Marut, all goes back to our goal which is
-            to create a space that is inclusive of all, enjoining and encouraging
-            the good.
-        </Typography>
-        <Typography variant="h6" align="center">Every Tuesday at 7:30pm</Typography>
+        <div id="landing-page">
+            <div>
+                <Typography variant="h2" align="center" style={{ marginTop: "7%" }}>
+                    Maruf Community
+                </Typography>
+                <Typography
+                    variant="h6"
+                    align="center"
+                    style={{
+                        paddingLeft: "3%",
+                        paddingRight: "3%",
+                        marginTop: "3%",
+                        fontWeight: "bold",
+                    }}
+                >
+                    Our community's name "Maruf" reflects our commitment to promoting
+                    the good. We believe that the prophetic teachings offer valuable
+                    guidance on how to live a fulfilling life, and we strive to
+                    incorporate these teachings into our daily practices. At Maruf, we
+                    foster a healthy social and learning environment by providing weekly
+                    sessions where members can engage in meaningful discussions, develop
+                    new skills, and support each other's growth. For instance, these
+                    sessions have sparked lively discussions and deepened our
+                    understanding of different cultures and perspectives. Ultimately,
+                    our goal is to create an inclusive space where everyone feels
+                    welcomed and valued, and where positive actions are encouraged and
+                    celebrated.
+                </Typography>
+                <Typography variant="h6" align="center" style={{marginTop: '5%'}}>
+                    Every Tuesday at 7:30pm
+                </Typography>
+            </div>
         </div>
-    </div>
     );
 }
 
