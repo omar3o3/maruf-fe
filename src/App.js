d67a2ddf4ad075159af973411ac510810c52f733
omar3o3 <otantawy53@gmail.com>
2023-03-18 22:55:38 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import LandingPage from "./components/LandingPage.js";
+import About from "./components/About.js";
+import NavBar from "./components/NavBar.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/About" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/LandingPage.js
@@ -0,0 +1,56 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+import bgImage from "../images/main-bg.JPG";
+import bgImage2 from "../images/bg-pic.png";
+import {
+    createTheme,
+    ThemeProvider,
+    styled,
+    responsiveFontSizes,
+} from "@mui/material/styles";
+
+function LandingPage() {
+    let theme = createTheme();
+
+    theme.typography.h6 = {
+        fontSize: "1.2rem",
+        "@media (min-width:600px)": {
+            fontSize: "1.5rem",
+        },
+        [theme.breakpoints.down("md")]: {
+            fontSize: "3rem",
+        },
+    };
+
+    return (
+    <div id="landing-page" >
+        <div>
+        <Typography
+            variant="h2"
+            align="center"
+            style={{ marginTop: "7%" }}
+        >
+            Maruf Community
+        </Typography>
+        <Typography variant="h6" align="center"
+            style={{
+                paddingLeft: "3%",
+                paddingRight: "3%",
+                marginTop: "3%",
+                fontWeight: "bold",
+            }}>
+            We chose the word "Maruf" to be the name of our community because at
+            the core it means something that is well known to be good, which is
+            what we strive for. The prophetic teachings, healthy social and
+            learning environment, and most importantly the genuine companionship
+            developed and experienced at Marut, all goes back to our goal which is
+            to create a space that is inclusive of all, enjoining and encouraging
+            the good.
+        </Typography>
+        <Typography variant="h6" align="center">Every Tuesday at 7:30pm</Typography>
+        </div>
+    </div>
+    );
+}
+
+export default LandingPage;
