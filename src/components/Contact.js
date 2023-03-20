231c56259cd65ce141df230b47b57784f9d3715d
omar3o3 <otantawy53@gmail.com>
2023-03-20 15:35:54 -0400

    updated contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,14 +10,14 @@ function Contact() {
             <Typography variant="h2" align="center" style={{ marginTop: "7%" }}>
                 Reach Out To Us!
             </Typography>
-            <Typography variant="h6" align="center" style={{ marginTop: "2%" }}>
-                We will try our best to answer all messages as soon as possible.
-            </Typography>
-            <Typography variant="h3" align="center" style={{ marginTop: "2%" }}>
+            <Typography variant="h4" align="center" style={{ marginTop: "2%" }}>
                 Email us at
             </Typography>
-            <Typography variant="h3" align="center" style={{ marginTop: "2%" }}>
+            <Typography variant="h4" align="center" style={{ marginTop: "2%" }}>
                 maruficsi@gmail.com
+            </Typography>
+            <Typography variant="h6" align="center" style={{ marginTop: "2%" }}>
+                We will try our best to answer all inquiries as soon as possible.
             </Typography>
         </div>
     );
